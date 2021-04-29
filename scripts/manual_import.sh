cd9de290ba5f7da93b78fa08ec2ff42e89e3a3e8
André Carvalho <afacarvalho@yahoo.com.br>
2021-04-29 12:27:23 -0300

    Changes on manual import file

--- scripts/manual_import.sh
+++ scripts/manual_import.sh
@@ -3,8 +3,5 @@
 source /etc/environment
 cd $INSTALL_PATH/scripts-shell
 
-echo "DETER_R_AMZ_CR2_2021-04-20_2021-04-20.zip" > "$INPUT_DIR/trigger.txt"
-. ./import_shapes_to_database.sh
-
-echo "DETER_R_AMZ_CR2_2021-04-21_2021-04-21.zip" > "$INPUT_DIR/trigger.txt"
+echo "DETER_R_AMZ_CR2_2021-04-27_2021-04-27.zip" > "$INPUT_DIR/trigger.txt"
 . ./import_shapes_to_database.sh
