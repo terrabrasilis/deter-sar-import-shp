f15c671e111b632bd8b7ad87f1cbe8f3b65a0bb0
André Carvalho <afacarvalho@yahoo.com.br>
2021-04-29 12:25:53 -0300

    Fix sendmail paths to read log file

--- scripts/send-mail.sh
+++ scripts/send-mail.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 DATE_LOG=$(date +%Y-%m-%d)
-LOGFILE="terrama_ftp_push_$DATE_LOG.log"
+LOGFILE="import_shapefile_$DATE_LOG.log"
 
-TO=$(cat "$STATIC_FILES_DIR"/mail_to.cfg )
-BODY="$STATIC_FILES_DIR/$LOGFILE"
+TO=$(cat "$SHARED_DIR"/mail_to.cfg )
+BODY="$SHARED_DIR/logs/$LOGFILE"
 (cat - $BODY)<<HEADERS_END | /usr/sbin/sendmail -F "TerraBrasilis" -i $TO
-Subject: [TerraBrasilis] - Log do envio de dados DETER-MT para o FTP
+Subject: [DETER-R] - Log de importação de dados para validação
 To: $TO
 
 HEADERS_END
