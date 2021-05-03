b8788d48c4ed87a2633a1e3aa9797d89828d8172
André Carvalho <afacarvalho@yahoo.com.br>
2021-05-03 16:27:39 -0300

    Adding From information into mail header

--- scripts/send-mail.sh
+++ scripts/send-mail.sh
@@ -3,6 +3,7 @@ TO=$(cat "$SHARED_DIR"/mail_to.cfg )
 BODY="$1"
 (cat - $BODY)<<HEADERS_END | /usr/sbin/sendmail -F "TerraBrasilis" -i $TO
 Subject: [DETER-R] - Log de importacao de dados para validacao
+From: "TerraBrasilis<terrabrasilis@inpe.br>"
 To: $TO
 
 HEADERS_END
