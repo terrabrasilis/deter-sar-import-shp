6dfe9d87db51f32f05ab07b78f1dee186774cc6c
André Carvalho <afacarvalho@yahoo.com.br>
2021-05-10 14:30:37 -0300

    Fix email script

--- scripts/send-mail.sh
+++ scripts/send-mail.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+BODY="$1"
+
 # if email control is enable
 if [ $EMAIL_CTRL = true ]; then
 
 TO=$(cat "$SHARED_DIR"/mail_to.cfg )
-BODY="$1"
 (cat - $BODY)<<HEADERS_END | /usr/sbin/sendmail -F "TerraBrasilis" -i $TO
 Subject: [DETER-R] - Log de importacao de dados para validacao
 From: "terrabrasilis@inpe.br"
@@ -14,6 +15,6 @@ HEADERS_END
 
 else
   echo "------------- email content start --------------"
-  echo "$BODY"
+  echo $(cat $BODY)
   echo "------------- email content end --------------"
 fi
