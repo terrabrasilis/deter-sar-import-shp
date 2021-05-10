ada5e0a7180519b7f7ca12d7617fcbe5c1027558
André Carvalho <afacarvalho@yahoo.com.br>
2021-05-10 12:06:00 -0300

    Implements the control of sending e-mail

--- scripts/send-mail.sh
+++ scripts/send-mail.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# if email control is enable
+if [ $EMAIL_CTRL = true ]; then
+
 TO=$(cat "$SHARED_DIR"/mail_to.cfg )
 BODY="$1"
 (cat - $BODY)<<HEADERS_END | /usr/sbin/sendmail -F "TerraBrasilis" -i $TO
@@ -7,3 +11,9 @@ From: "terrabrasilis@inpe.br"
 To: $TO
 
 HEADERS_END
+
+else
+  echo "------------- email content start --------------"
+  echo "$BODY"
+  echo "------------- email content end --------------"
+fi
