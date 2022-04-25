81ca283d81e2237ede75d566011cd30a5fa5fae5
andre-carvalho <afacarvalho@yahoo.com.br>
2022-04-25 13:55:07 -0300

    New entries in manual import script

--- scripts/manual_import.sh
+++ scripts/manual_import.sh
@@ -6,82 +6,74 @@ cd $INSTALL_PATH/scripts-shell
 # disable sendmail
 export EMAIL_CTRL=false
 
-echo "DETER_R_AMZ_CR2_2021-04-20_2021-04-20.zip" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-04-21"
+echo "DETER_R_AMZ_CR2_2022-04-04_2022-04-04.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-05"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-04-21_2021-04-21.zip" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-04-22"
+echo "DETER_R_AMZ_CR2_2022-04-05_2022-04-05.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-06"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-04-22_2021-04-22.zip" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-04-23"
+echo "DETER_R_AMZ_CR2_2022-04-06_2022-04-06.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-07"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-04-23_2021-04-23.zip" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-04-24"
+echo "DETER_R_AMZ_CR2_2022-04-07_2022-04-07.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-08"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-04-24_2021-04-24.zip" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-04-25"
+echo "DETER_R_AMZ_CR2_2022-04-09_2022-04-09.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-10"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-04-25_2021-04-25.zip" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-04-26"
+echo "DETER_R_AMZ_CR2_2022-04-10_2022-04-10.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-11"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-04-26_2021-04-26.zip" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-04-27"
+echo "DETER_R_AMZ_CR2_2022-04-11_2022-04-11.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-12"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-04-27_2021-04-27.zip" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-04-28"
+echo "DETER_R_AMZ_CR2_2022-04-12_2022-04-12.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-13"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-04-28_2021-04-28.zip" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-04-29"
+echo "DETER_R_AMZ_CR2_2022-04-13_2022-04-13.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-14"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-04-29_2021-04-29.geojson" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-04-30"
+echo "DETER_R_AMZ_CR2_2022-04-14_2022-04-14.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-15"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-05-01_2021-05-01.geojson" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-05-02"
+echo "DETER_R_AMZ_CR2_2022-04-15_2022-04-15.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-16"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-05-02_2021-05-02.geojson" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-05-03"
+echo "DETER_R_AMZ_CR2_2022-04-16_2022-04-16.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-17"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-05-03_2021-05-03.geojson" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-05-04"
+echo "DETER_R_AMZ_CR2_2022-04-18_2022-04-18.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-19"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-05-04_2021-05-04.geojson" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-05-05"
+echo "DETER_R_AMZ_CR2_2022-04-19_2022-04-19.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-20"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-05-05_2021-05-05.geojson" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-05-06"
+echo "DETER_R_AMZ_CR2_2022-04-20_2022-04-20.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-21"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-05-06_2021-05-06.geojson" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-05-07"
+echo "DETER_R_AMZ_CR2_2022-04-21_2022-04-21.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-22"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-05-07_2021-05-07.geojson" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-05-08"
+echo "DETER_R_AMZ_CR2_2022-04-22_2022-04-22.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-23"
 . ./import_to_database.sh
 
-echo "DETER_R_AMZ_CR2_2021-05-08_2021-05-08.geojson" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-05-09"
-. ./import_to_database.sh
-
-echo "DETER_R_AMZ_CR2_2021-05-09_2021-05-09.geojson" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-05-10"
-. ./import_to_database.sh
-
-echo "DETER_R_AMZ_CR2_2021-05-10_2021-05-10.geojson" > "$INPUT_DIR/trigger.txt"
-export CREATED_AT="2021-05-11"
+echo "DETER_R_AMZ_CR2_2022-04-23_2022-04-23.geojson" > "$INPUT_DIR/trigger.txt"
+export CREATED_AT="2022-04-24"
 . ./import_to_database.sh
